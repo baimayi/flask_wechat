2ee2f6d4ae3edac957a9f8bcd930dd546585236b
baimayi <1920571248@qq.com>
2019-05-12 16:33:39 +0800

    Add wechat interface

--- web/controllers/static.py
+++ web/controllers/static.py
@@ -1,11 +1,8 @@
-# -*- encoding: utf-8 -*-
-
-# 实际部署不需要
+# -*- coding: utf-8 -*-
 from flask import Blueprint,send_from_directory
 from application import app
+route_static = Blueprint('static', __name__)
 
-root_static = Blueprint('static', __name__)
-
-@root_static.route('/<path:filename>')
-def index(filename):
-    return send_from_directory(app.root_path + "/web/static/", filename)
+@route_static.route("/<path:filename>")
+def index( filename ):
+    return send_from_directory(  app.root_path + "/web/static/" ,filename )
